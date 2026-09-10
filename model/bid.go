c81281de853c287d0293088dfbaa62ef9ff480ec
Xueria <180748723+Xueria@users.noreply.github.com>
2026-09-10 15:15:34 +0800

    refactor: 移除 BidItem 与 1x1 价格数据中的 ID 字段

--- model/bid.go
+++ model/bid.go
@@ -1,8 +1,7 @@
 package model
 
-// BidItem 竞拍单格中的一件拍品：标识、名称与该格的成交价格。
+// BidItem 竞拍单格中的一件拍品：名称与该格的成交价格。
 type BidItem struct {
-	ID    string `json:"id"`
 	Name  string `json:"name"`
 	Value int    `json:"value"`
 }
